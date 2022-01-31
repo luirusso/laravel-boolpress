1c21a4b1d289daad40cd0ffc34101a4d74a2c067
luirusso <luirusso_@hotmail.it>
2022-01-31 17:09:46 +0100

    added first vue component

---
+++ resources/js/views/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <h1>
+            Work in progress
+        </h1>
+        <h2>
+            Sito in costruzione
+        </h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App',
+}
+</script>
+
+<style lang="scss">
+
+</style>
