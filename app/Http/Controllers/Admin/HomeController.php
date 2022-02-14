eee2fe8d2a11764c55ecf50b4b78dc5577603f66
luirusso <luirusso_@hotmail.it>
2022-02-14 18:53:23 +0100

    added automated email when loading admin home

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Mail\SendWelcomeEmail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -11,7 +14,17 @@ class HomeController extends Controller
      * Admin Dashboard
      */
 
+    
     public function index() {
+        // TEST EMAIL
+
+        // STATIC DATA
+        // Mail::to('account@mail.it')->send(new SendWelcomeEmail() );
+
+        // WITH AUTH
+        Mail::to(Auth::user()->email)->send(new SendWelcomeEmail() );
+
+
         return view('admin.home');
     }
 }
---
+++ resources/views/mails/welcome.blade.php
@@ -0,0 +1,3 @@
+<h1>Welcome!</h1>
+
+<p>Hello there, welcome to our site!</p>
